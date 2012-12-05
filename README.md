edaf1d99746def5c7a2c3556e1237a8cd6cdbdc0
Michał Kowalkowski <kowalkowski.michal@gmail.com>
2012-12-05 17:03:42 +0800

    Uploading files

--- README.md
+++ README.md
@@ -1,4 +1,138 @@
 betterCheckbox
-==============
-
+========
 betterCheckbox is a jquery plugin that allows you to customize your checkbox forms using CSS.
+
+Browser Support
+--------
+
+betterCheckbox has been tested and should work in
+- Internet Explorer 9
+- Safari
+- Firefox
+- Chrome
+- Opera
+
+Examples
+--------
+
+You can find some examples [here](http://www.michalkowalkowski.com/betterCheckbox).
+
+Usage
+--------
+
+If you want to use default style include betterCheckbox and its stylesheet on your page before `</body>` tag
+
+```html
+<link href="betterCheckbox.css" type="text/css" rel="stylesheet">
+<script src="/path/to/betterCheckbox.min.js"></script>
+```
+  			
+Then just add it to one of DOMs elements
+
+```html
+<script>
+$('#elementsID').betterCheckbox();
+</script>
+```			
+
+
+API
+--------
+Settable Options
+<table>
+<thead>
+<tr>
+<th>Option</th>
+<th>Description</th>
+<th>Data Type</th>
+<th>Default</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>boxClass</td>
+<td>Name of the class you want to use for the checkbox</td>
+<td>string</td>
+<td>'betterCheckbox-box'</td>
+</tr>						
+<tr>
+<td>tickClass</td>
+<td>Name of the class you want to use for the checkmark</td>
+<td>string</td>
+<td>'betterCheckbox-tick'</td>
+</tr>						
+<tr>
+<td>tickInnerHTML</td>
+<td>Set innerHTML for checkmark div (you can use text as a checkmark)</td>
+<td>string</td>
+<td>' '</td>
+</tr>						
+<tr>
+<td>disabledBoxClass</td>
+<td>Name of the class you want to add to the checkbox when disabled</td>
+<td>string</td>
+<td>'disabled'</td>
+</tr>						
+<tr>
+<td>disabledTickClass</td>
+<td>Name of the class you want to add to the checkmark when disabled</td>
+<td>string</td>
+<td>'disabled'</td>
+</tr>							
+</tbody>
+</table>
+
+Methods
+
+<table>
+<thead>
+<tr>
+<th>Method</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>disable</td>
+<td>Disables the checkbox</td>
+</tr>						
+<tr>
+<td>enable</td>
+<td>Enables the checkbox</td>
+</tr>						
+<tr>
+<td>check</td>
+<td>Checks the checkbox</td>
+</tr>						
+<tr>
+<td>uncheck</td>
+<td>Unchecks the checkbox</td>
+</tr>						
+<tr class="last">
+<td>destroy</td>
+<td>Removes plugin functionality</td>
+</tr>						
+</tbody>
+</table>
+
+Other checkbox plugins
+-------
+If you don't want to style checkbox yourself, or you are looking for some specific look, try those plugins:
+[Fancy checkboxes and radio buttons](http://webdesign.maratz.com/lab/fancy-checkboxes-and-radio-buttons/demo.html)
+[jqTransform](http://www.dfc-e.com/metiers/multimedia/opensource/jqtransform/)
+[jQuery checkbox](http://widowmaker.kiev.ua/checkbox/)
+[iOS Checkboxes](http://ios-checkboxes.awardwinningfjords.com/)
+[Niceforms](http://www.emblematiq.com/lab/niceforms/demo/niceforms.html)
+[jQuery Fancy Custom Radio-button and Checkbox](http://blogs.digitss.com/javascript/jquery-javascript/jquery-fancy-custom-radio-and-checkbox/)
+
+Coming Soon
+-------
+betterRadio
+				
+License
+-------
+betterCheckbox plugin is released under [MIT license](http://opensource.org/licenses/mit-license.php).
+
+Credits
+-------
+betterCheckbox plugin was created by [Michał Kowalkowski](https://github.com/michalkow). You can contact me at [kowalkowski.michal@gmail.com](mailto:kowalkowski.michal@gmail.com)
---
+++ betterCheckbox.js
@@ -0,0 +1,127 @@
+(function( $ ){
+
+  var methods = {
+    init : function( options ) {
+	 
+	// Default settings 
+	 var settings = $.extend( {
+      boxClass : 'betterCheckbox-box',
+	  tickClass : 'betterCheckbox-tick',
+	  tickInnerHTML : '',
+	  disabledBoxClass : 'disabled',
+	  disabledTickClass : 'disabled',
+    }, options);
+
+       return this.each(function(){
+			var root = this;
+			var $this=$(this);
+			root.checked=root.checked;
+			root.disabled=root.disabled;
+			root.bgclass=settings.boxClass;
+			root.tickclass=settings.tickClass;
+			root.tickinner=settings.tickInnerHTML;
+			root.disbgclass=settings.disabledBoxClass;
+			root.distickclass=settings.disabledTickClass;
+			
+			$this.css('display', 'none');
+			$this.after('<div class="'+settings.boxClass+'"></div>');
+			$this.next().css('user-select', 'none');
+			if(root.checked) {
+				$this.next().append('<div class="'+settings.tickClass+'">'+settings.tickInnerHTML+'</div>');
+			}
+			if(root.disabled) {
+				$this.next().addClass(root.disbgclass);
+				$this.next().children().addClass(root.distickclass);
+			}
+			
+			$this.next().on('click', function() {
+				if(!root.disabled) {
+					if(root.checked) {
+						$(this).empty();
+						root.checked = false;
+						$this.prop('checked', false);
+					} else {
+						$(this).append('<div class="'+settings.tickClass+'">'+settings.tickInnerHTML+'</div>');
+						root.checked = true;
+						$this.prop('checked', true);
+					}
+				}
+			});		
+			return this;				
+       });
+    },
+	
+    destroy : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				$this.css('display', 'inline-block');
+				$this.next().remove();
+				return this;
+       });
+    },
+	
+	disable : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				$this.next().addClass(root.disbgclass);
+				$this.next().children().addClass(root.distickclass);
+				root.disabled=true;
+				$this.prop('disabled', true);
+				return this;
+       });
+    },
+
+	enable : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				$this.next().removeClass(root.disbgclass);
+				$this.next().children().removeClass(root.distickclass);
+				root.disabled=false;
+				$this.prop('disabled', false);
+				return this;
+       });
+    },
+	
+	check : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				$this.next().append('<div class="'+root.tickclass+'">'+root.tickinner+'</div>');
+				if(root.disabled) {
+					$this.next().children().addClass(root.distickclass);
+				}
+				root.checked = true;
+				$this.prop('checked', true);
+				return this;
+       });
+    },
+	
+	uncheck : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				$this.next().empty();
+				root.checked = false;
+				$this.prop('checked', false);
+				return this;
+       });
+    },
+	
+  };
+
+  $.fn.betterCheckbox = function( method ) {
+
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.betterCheckbox' );
+    }    
+  
+  };
+
+})( jQuery );
